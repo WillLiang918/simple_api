976dd27236f8483abaf0116611e8d8938fe2b3c5
William Liang <williamliang918@gmail.com>
2017-08-08 23:15:37 -0400

    refactor SimpleApi get method to accept params

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var jqueryParam = require('jquery-param');
 var rp = require('request-promise');
 
 module.exports = class SimpleApi {
@@ -11,24 +12,30 @@ module.exports = class SimpleApi {
   }
 
   get(path, params = {}) {
+    return this._query('GET', path, params);
+  }
+
+  // Private Methods
+
+  _query(method, path, params) {
+    var requestOptions = {
+      method: method,
+      url: this._buildURL(path, params),
+      headers: this.headers
+    };
+
+    return rp(requestOptions);
+  }
+
+  _buildURL(path, parameters) {
     var url;
 
     if (this.default_host) {
       url = this.default_host + path;
     } else {
       url = path;
-    }
-
-    return this.query('GET', url, params);
-  }
-
-  query(method, url, params) {
-    var requestOptions = {
-      method: method,
-      url: url,
-      headers: this.headers
     };
 
-    return rp(requestOptions);
+    return `${url}?${jqueryParam(parameters)}`;
   }
-}
+};
